343396fb15b516bdf737f9d808315602bc443e8c
vikash-agrahari <vikash.agrahari@appinventiv.com>
2024-03-15 13:18:45 +0530

    moved firebaseKey to env

--- src/providers/firebase/firebase.module.ts
+++ src/providers/firebase/firebase.module.ts
@@ -5,18 +5,16 @@ import * as admin from 'firebase-admin';
 import { resolve } from 'path';
 import { EntityModule } from 'src/entity/entity.module';
 
-
 @Module({
   imports: [ConfigModule.forRoot(), EntityModule],
   providers: [FirebaseService],
   exports: [FirebaseService],
 })
 export class FirebaseModule {
-  constructor() {
-    const env = process.env.NODE_ENV || false;
-    if (!env) process.exit(100);
+  constructor(private readonly config : ConfigService) {
+    const firebaseKey:string = this.config.get('FIREBASE_CONFIG') as string;
     admin.initializeApp({
-      credential: admin.credential.cert(resolve(process.cwd(), `firebase.${env}.json`)),
+      credential: admin.credential.cert(JSON.parse(firebaseKey)),
     });
   }
 }
